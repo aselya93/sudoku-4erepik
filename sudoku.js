c1e4b1abe17875a51b0b18d6f6d7c38a7f252874
dxpemxnk <petvik5@yandex.ru>
2024-10-18 13:22:23 +0300

    solve_wip

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,50 @@
+
+
+
 function read() {
   /**
    * Прочесть файл puzzles.txt в кодировке 'utf-8' и вернуть эти данные из функции
    */
 }
 
-function solve() {
+
+
   /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+     * Принимает игровое поле в том формате, в котором его вернули из функции read.
+     * Возвращает игровое поле после попытки его решить.
+     */
+function solve(arr) {
+
+  // переделка массива
+  for (let i = 0; i < arr.length; i++) {
+    const arri = arr[i]
+    for (let j = 0; j < arr.length; j++) {
+  
+      if (arri[j]==='-')
+      {
+        arri[j]=0
+      }
+      else if(typeof(arri[j]==='string'))
+      {
+        arri[j]=Number(arri[j])
+      
+      }
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    const arri = arr[i]
+    for (let j = 0; j < arr.length; j++) {
+        if(arri[j]===0 && arri.includes(i)){
+          console.log(111111)
+          arri[j]=i+1
+        }
+    }
+  }
+  
+  
+  return arr
 }
+console.log(solve(arr))
 
 function isSolved() {
   /**
@@ -25,3 +60,5 @@ function prettyBoard() {
    * Подумай, как симпатичнее его вывести.
    */
 }
+
+
